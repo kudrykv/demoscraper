7008c1cecd1c975967ba2111e7a3f54e114d89ff
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-07-17 09:15:51 +0300

    splitting in smaller pieces

--- internal/core/crawler.go
+++ internal/core/crawler.go
@@ -27,43 +27,51 @@ func (r *Crawler) Crawl(ctx context.Context, parameters CrawlParameters) (<-chan
 		return nil, fmt.Errorf("link from raw url: %w", err)
 	}
 
-	hostname := link.Hostname()
-
 	processedCrawlEntriesChan := make(chan entities.CrawlEntry, 1)
-
-	processedLinks := entities.Links{link}
-	visitedMap := make(map[string]struct{})
 
 	go func() {
 		defer close(processedCrawlEntriesChan)
 
-		for depth := 1; depth <= parameters.DepthLimit; depth++ {
-			webPages := r.webPager.NewFromLinks(processedLinks)
-			processedLinks = processedLinks[:0]
-
-			for _, page := range webPages {
-				if err := page.Load(ctx); err != nil {
-					continue
-				}
-
-				links, err := page.Links(ctx)
-				if err != nil {
-					continue
-				}
-
-				links = links.SupplementMissingHostname(link).FilterHostname(hostname).Cleanup().Unique().DropVisited(visitedMap)
-				visitedMap = r.merge(visitedMap, links.ToVisitedMap())
-
-				for _, entry := range links.ToCrawlEntries(depth) {
-					processedCrawlEntriesChan <- entry
-				}
-
-				processedLinks = append(processedLinks, links...)
-			}
-		}
+		r.process(ctx, parameters, link, processedCrawlEntriesChan)
 	}()
 
 	return processedCrawlEntriesChan, nil
+}
+
+func (r *Crawler) process(
+	ctx context.Context,
+	parameters CrawlParameters,
+	link entities.Link,
+	processedCrawlEntriesChan chan<- entities.CrawlEntry,
+) {
+	hostname := link.Hostname()
+	processedLinks := entities.Links{link}
+	visitedMap := make(map[string]struct{})
+
+	for depth := 1; depth <= parameters.DepthLimit; depth++ {
+		webPages := r.webPager.NewFromLinks(processedLinks)
+		processedLinks = processedLinks[:0]
+
+		for _, page := range webPages {
+			if err := page.Load(ctx); err != nil {
+				continue
+			}
+
+			links, err := page.Links(ctx)
+			if err != nil {
+				continue
+			}
+
+			links = links.SupplementMissingHostname(link).FilterHostname(hostname).Cleanup().Unique().DropVisited(visitedMap)
+			visitedMap = r.merge(visitedMap, links.ToVisitedMap())
+
+			for _, entry := range links.ToCrawlEntries(depth) {
+				processedCrawlEntriesChan <- entry
+			}
+
+			processedLinks = append(processedLinks, links...)
+		}
+	}
 }
 
 func (r *Crawler) merge(left map[string]struct{}, right map[string]struct{}) map[string]struct{} {
